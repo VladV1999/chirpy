9000dec28d4736f811f95cdcda1b16143f484d76
Vlad <vlad.voskanyan@gmail.com>
2026-03-05 22:56:21 -0500

    Add basic handling for get

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,14 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.get("/healthz", handlerReadiness);
+export async function handlerReadiness(req, res) {
+    res.set({
+        'Content-Type': 'text/plain; charset=utf-8'
+    });
+    res.send('OK');
+}
+app.use("/app", express.static("./src/app"));
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
-
+import { handlerReadiness } from "./api/readiness";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.get("/healthz", handlerReadiness);
+
+app.use("/app", express.static("./src/app"));
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
