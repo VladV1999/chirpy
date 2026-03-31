8d831b709dfc453dc316dad92e8dc072c03f4fa0
Vlad <vlad.voskanyan@gmail.com>
2026-03-31 14:44:40 -0400

    Add tests and authentication for users

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,5 +1,8 @@
 import * as argon2 from "argon2";
+import type { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
 export async function hashPassword(password: string): Promise<string> {
     try {
         const hash = await argon2.hash(password);
@@ -19,4 +22,32 @@ export async function checkPasswordHash(password: string, hash: string): Promise
     } catch (err) {
         throw new Error("Something went wrong trying to verify password");
     }
+}
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
+    const now = Math.floor(Date.now() / 1000);
+    const payload: payload = {
+        iss: "chirpy",
+        sub: userID,
+        iat: now,
+        exp: now + expiresIn,
+    }
+    const token = jwt.sign(payload, secret);
+    return token;
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+    let result: string | JwtPayload = "";
+    try {
+        result = jwt.verify(tokenString, secret);
+    } catch (err) {
+        throw new Error("The token is invalid!");
+    }
+    if (typeof result === "string") {
+        throw new Error("Something unexpected happened, the payload is a string!");
+    }
+    if (result.sub === undefined) {
+        throw new Error("The user's id is undefined!");
+    }
+    return result.sub;
 }
